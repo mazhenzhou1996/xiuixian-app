b7d5d13f088b1d42f07b8ca7cc0e3997be7a643c
mazhenzhou1996 <35602794+mazhenzhou1996@users.noreply.github.com>
2026-08-15 15:16:00 +0800

    deploy: xiuixian v35 production build (2026-08-15)

---
+++ assets/skeleton-Bt-UZ_Z9.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-BiVfxGNB.js";import{c as s}from"./index-BW-b5by9.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
